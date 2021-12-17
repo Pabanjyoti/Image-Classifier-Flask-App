bff3716dc32cb3677be2c5e437667481edf7fe08
Pabanjyoti <pabanjborah@gmail.com>
2021-12-17 20:24:31 +0530

    added rcnn

--- app.py
+++ app.py
@@ -4,16 +4,9 @@ Created on Sat Dec 11 13:17:51 2021
 
 @author: DELL
 """
-
+import alexnet
+import fasterRcnn
 from flask import Flask, render_template, request
-
-import torch
-import torchvision.models as models
-import requests, io
-from torchvision import datasets, transforms as T
-from PIL import Image
-
-alexnet = models.alexnet(pretrained=True)
 
 app=Flask(__name__)
 
@@ -26,23 +19,17 @@ def hello():
 
 
 def predict():
-    img_url=request.form.get('image')
 
-    response=requests.get(img_url)
+    img_url = request.form.get('image')
+    nnModel = request.form.get('nnModel')
 
-    transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor(), T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
-    img_pil = Image.open(io.BytesIO(response.content))        #image goes here
-    img = transform(img_pil)
+    if nnModel == 'alexnet':
+        cls = alexnet.alex(img_url)
+    elif nnModel == 'fasterRcnn':
+        # cls = fasterRcnn.faster(img_url)
+        cls = alexnet.alex(img_url)
 
-    feed = torch.unsqueeze(img, 0)
-    out = alexnet(feed)
-
-    with open('./imagenet_classes.txt') as f:
-        classes = [line.strip() for line in f.readlines()]
-    
-    _, indices = torch.sort(out, descending=True)
-    percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
-    cls = [(classes[idx], percentage[idx].item()) for idx in indices[0][:3]]
+    #fasterRcnn.faster(img_url)                 #for faster rcnn
 
     return render_template('home.html', pred=cls)
 
