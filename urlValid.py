14e861d76ac0864a4e544c4a2cb467dcef6c7a49
Pabanjyoti <pabanjborah@gmail.com>
2022-02-22 00:22:34 +0530

    modified json response

--- urlValid.py
+++ urlValid.py
@@ -5,7 +5,7 @@ def valid_url(url):
     response = requests.get(url)
     mimetype = response.headers.get("Content-Type", default = None)
 
-    if mimetype:
-        return any([mimetype.startswith("image")])
+    if any([mimetype.startswith("image")]) == True:
+        return mimetype
     else:
-        return False
+        return 'invalid_image_url'
